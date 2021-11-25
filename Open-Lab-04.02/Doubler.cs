335df94ad133d0cb84170e3444290518feb9e0b0
Samuel Strýček <theghostband@outlook.com>
2021-11-25 08:57:43 +0100

    created new string

--- Open-Lab-04.02/Doubler.cs
+++ Open-Lab-04.02/Doubler.cs
@@ -6,7 +6,18 @@ namespace Open_Lab_04._02
     {
         public string DoubleChar(string original)
         {
-            throw new NotImplementedException();
+            int position = 0;
+            char[] characters = original.ToCharArray();
+            char[] new_characters = new char[characters.Length * 2];
+            foreach (char character in original)
+            {
+                new_characters[position] = character;
+                new_characters[position + 1] = character;
+                position += 2;
+            }
+            {
+                return new string(new_characters);
+            }
         }
     }
 }
